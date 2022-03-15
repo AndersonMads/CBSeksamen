3e6f4cd7e2ebe2344a3ff57d6cb6decc19eba59b
AndersonMads <91493130+AndersonMads@users.noreply.github.com>
2022-03-15 13:10:27 +0100

    Opsætning til SQL DB med JS

---
+++ Database/db_connect.js
@@ -0,0 +1,38 @@
+var Connection = require('tedious').Connection
+var Request = require('tedious').Request
+
+const config = require('./config.json')
+var connection = new Connection(config)
+
+
+connection.on('connect', function(err){
+    if (err){
+        console.log(err);
+    } else {
+        console.log("connected");
+        const response = executeSQL()
+        console.log(response);
+    }
+});
+
+connection.connect()
+
+
+
+function executeSQL(){
+    Request = new Request("SELECT * FROM dbo.jobs", function(err){
+    if (err){
+        console.log(err)}})
+
+    connection.execSql(Request)
+    var counter = 1
+    response = {}
+    Request.on('row', function(columns){
+        response[counter] = {}
+        columns.forEach(function(column){
+            response[counter][column.metadata.colName] = column.value
+        });
+        counter += 1
+    });
+    return response
+};
