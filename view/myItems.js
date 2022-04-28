5596426536c70e07c33dcd7a5c740ada86a112ab
albertsteenstrup21 <alst21ag@student.cbs.dk>
2022-04-28 11:14:47 +0200

    Update og Delete item nu inde på brugers item side

--- view/myItems.js
+++ view/myItems.js
@@ -50,5 +50,71 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    let submitButton = document.getElementById('submit');
+
+    submitButton.addEventListener('click', function(e) {
+        // preventDefault sikrer at siden ikke opdatere imens form input oplyses
+        e.preventDefault();
+
+        let usernameInput = document.getElementById('username').value;
+
+        let deletedItem = {
+            username: usernameInput,
+        }
+        
+        //Poster givne oplysninger
+        fetch('http://localhost:3000/deleteOwnItems', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(deletedItem)
+        })
+        .then(response => {
+            window.alert('Item deleted');
+            location.href = "/myItems.html";     
+        })
+        .catch((error) => {
+            console.log('Error:', error)
+        })
+    });
+
+    let submitButtonUpdate = document.getElementById('submitUpdate');
+
+    submitButtonUpdate.addEventListener('click', function(e) {
+        // preventDefault sikrer at siden ikke opdatere imens form input oplyses
+        e.preventDefault();
+
+       let priceInput = document.getElementById('price').value;
+        let itemNameInput = document.getElementById('i_name').value;
+       let categoryInput = document.getElementById('category_id').value;
+       let reusablesInput = document.getElementById('reusables').value;
+        let itemId = document.getElementById('id').value;
+
+        let newItem = {
+            itemName: itemNameInput,
+            id: itemId,
+            price: priceInput,
+            category: categoryInput,
+            reusable: reusablesInput
+        }
+        
+        //Poster givne oplysninger
+        fetch('http://localhost:3000/updateOwnItems', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newItem)
+        }).then(response => response.json())
+        .then(() => {
+            window.alert('Item updated'); 
+            location.href = "/myItems.html";  
+        })
+        .catch((error) => {
+            console.log('Error:', error)
+        })
+    });
+
 });
 
